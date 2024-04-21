000281d3b95fb5421b6f83f231b9aa1678e33ef8
agil <mughni@mhs.usk.ac.id>
2024-04-21 14:43:02 +0700

    feat: add config font and color

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,22 @@ export default {
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
     "./resources/**/*.vue",
+
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        "linen": "#FFF9F0",
+        "light": "#F0F2F2",
+        "navy": "#2E4A70",
+        "blue": "#D1E9FF",
+        "black": "#161C2D",
+        "blue-sky": "#73C7E3",
+      }
+    },
   },
   plugins: [],
 }
