a442635c25f23c2da771ff5880b2d8f23c30496d
agil <mughni@mhs.usk.ac.id>
2024-04-28 01:56:05 +0700

    feat: add header component

---
+++ resources/views/layouts/component/header.blade.php
@@ -0,0 +1,8 @@
+<div class="bg-navy text-light py-5">
+    <div class="container mx-auto p-10 font-poppins text-center flex flex-col gap-3">
+        <h2 class="text-[40px] font-semibold">{{ $pageTitle }}</h2>
+        <div class="max-w-md mx-auto">
+            <p class="text-lg font-normal">{{ $description }}</p>
+        </div>
+    </div>
+</div>
