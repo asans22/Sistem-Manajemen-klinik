d30374805e4e2f46c454698937324a0294bca942
agil <mughni@mhs.usk.ac.id>
2024-04-28 01:56:30 +0700

    feat: add home page

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -1,27 +1,33 @@
-@extends('layouts.layoutHome')
-@section('content')
+@extends ('layouts.layout')
+@section ('content')
 
-<!-- Elemen latar belakang dengan pseudo-element untuk gambar -->
-<div class="relative bg-black">
-    <div class="absolute inset-0 bg-[url('/public/assets/img/bg.jpg')] bg-cover bg-center opacity-[20.11%]"></div>
-    <!-- Konten utama -->
-    <div class="relative z-10 p-[15%] font-poppins">
-        <div class="flex flex-col font-poppins">
-            <div class="text-linen cursor-default mb-[5%]">
-                <h1 class="font-semibold text-[44px] tracking-[-2px] mb-[10px]">Selamat datang di klinik kami!</h1>
-                <p class="font-light text-[23px] ">Dapatkan akses cepat dan mudah ke informasi obat, reservasi, dan saran ahli untuk meningkatkan kualitas hidup Anda.</p>
+@include ('layouts.component.header', ['pageTitle' => 'Selamat Datang'], ['description' => 'Kami siap akan memberikan layanan kesehatan terbaik bagi Anda dan keluarga'])
+
+<div class="flex justify-center gap-10 text-center font-poppins">
+    <div class="w-[40%] hover:shadow-navy p-16 my-16 shadow-md rounded-lg border-solid border-[1px] border-blue flex flex-col justify-center items-center transform hover:scale-105 transition duration-300">
+        <div class="mx-24 pb-48">
+            <div class="mx-auto rounded-full bg-linen h-24 w-24 flex items-center justify-center mb-6">
+                <img src="{{ url('assets/img/img-upcoming-event.png') }}" alt="Logo" class="h-16">
             </div>
-            <div class="text-[24px]">
-                <button class="text-linen bg-navy border-[1px] border-navy hover:text-navy hover:bg-blue-sky active:bg-black active:text-linen focus:ring focus:ring-linen rounded-[10px] p-3 px-7">
-                    Log In
-                </button>
-                <button class="hover:text-navy hover:bg-linen border-[1px] text-light border-black active:bg-black active:text-linen focus:ring focus:ring-black rounded-[10px] p-3 px-5">
-                    Sign Up
-                </button>
+            <h5 class="mb-1 text-[18px] text-navy font-normal">Pemesanan Mendatang</h5>
+            <p class="mb-6 text-sm text-[16px] text-navy font-normal">Kelola</p>
+            <h1 class="text-2xl text-[20px] text-navy font-bold">Lakukan Reservasi</h1>
+        </div>
+    </div>
+    <div class="w-[40%] hover:shadow-navy p-16 my-16 shadow-md  rounded-lg border-solid border-[1px] border-blue flex flex-col justify-center items-center transform hover:scale-105 transition duration-300">
+        <div class="mx-24 pb-48">
+            <div class="mx-auto rounded-full bg-linen h-24 w-24 flex items-center justify-center mb-6">
+                <img src="{{ url('assets/img/img-list-obat.png') }}" alt="Logo" class="h-16">
             </div>
+            <h5 class="mb-1 text-[18px] text-navy font-normal">Obat</h5>
+            <p class="mb-6 text-sm text-[16px] text-navy font-normal">Detil</p>
+            <h1 class="text-2xl text-[20px] text-navy font-bold">Lihat Obat</h1>
         </div>
     </div>
 </div>
+
+
+
 
 
 @endsection
