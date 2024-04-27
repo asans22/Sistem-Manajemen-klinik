b24ed3334e1eee8b31708a5f011b5871eacca140
agil <mughni@mhs.usk.ac.id>
2024-04-28 01:13:29 +0700

    add new routes

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function (){
+    return view('Landing');
+});
+
+Route::get('/login', function (){
+    return view('Login');
+});
+
+Route::get('/register', function (){
+    return view('Register');
+});
+
+Route::get('/home', function (){
     return view('Home');
 });
