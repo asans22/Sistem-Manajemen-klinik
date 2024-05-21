1668db70d9885ae8383f3afecd92cf4990e9b125
agil <mughni@mhs.usk.ac.id>
2024-05-21 18:04:53 +0700

    feat: add doctor route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\halamanAdminController;
 use App\Http\Controllers\UserSessionControl;
+use App\Http\Controllers\DoctorController;
 use App\Http\Middleware\isGuest;
 use App\Http\Middleware\isLogin;
 use App\Http\Middleware\UserAkses;
@@ -15,12 +16,16 @@ Route::post('/registerUser', [UserSessionControl::class, 'create'])->middleware(
 Route::get('/userLogout', [UserSessionControl::class, 'logout']);
 
 Route::get('/home', [UserSessionControl::class, 'home'])->middleware([isLogin::class, UserAkses::class.':user']); // Perbaiki penamaan middleware dan parameter
-Route::get('/dokter', [UserSessionControl::class, 'dokter'])->middleware([isLogin::class, UserAkses::class.':dokter']); // Perbaiki penamaan middleware dan parameter
+Route::get('/dokter', [UserSessionControl::class, 'dokter'])->middleware([isLogin::class, UserAkses::class.':user']); // Perbaiki penamaan middleware dan parameter
+
+Route::get('/dokterView', [UserSessionControl::class, 'pasien'])->middleware([isLogin::class, UserAkses::class.':dokter']); // Perbaiki penamaan middleware dan parameter
 
 Route::get('/admin', [UserSessionControl::class, 'admin'])->middleware([isLogin::class,UserAkses::class.':admin']); // Perbaiki penamaan middleware dan parameter
 Route::get("/adminDokter",[halamanAdminController::class,'adminDokter'])->middleware([isLogin::class,UserAkses::class.':admin']);
 Route::get("/adminPasien",[halamanAdminController::class,'adminPasien'])->middleware([isLogin::class,UserAkses::class.':admin']);
 Route::get("/adminObat",[halamanAdminController::class,'adminObat'])->middleware([isLogin::class,UserAkses::class.':admin']);
+
+Route::resource('doctors', DoctorController::class);
 
 Route::get('/reservasi',function() {
     return view('/user/reservasi');
