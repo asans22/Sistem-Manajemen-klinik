862a623aacabacdf9fe83f12470d7e3234b57759
agil <mughni@mhs.usk.ac.id>
2024-05-29 14:04:18 +0700

    feat: add new routes

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,21 @@ Route::get('/userLogout', [UserSessionControl::class, 'logout']);
 //user
 Route::get('/home', [UserSessionControl::class, 'home'])->middleware([isLogin::class, UserAkses::class.':user']);
 Route::get('/dokterUser', [UserSessionControl::class, 'dokterUser'])->middleware([isLogin::class, UserAkses::class.':user']);
-Route::get('/myAccount', [UserSessionControl::class, 'myAccount'])->middleware([isLogin::class, UserAkses::class.':user']);
+Route::get('/reservasiDokter/{id?}', [UserSessionControl::class, 'reservasiDokter'])->middleware([isLogin::class, UserAkses::class.':user'])->name('reservasiDokter');
+Route::get('/get-doctors/{spesialis}', [UserSessionControl::class, 'getDoctorsBySpesialis'])->middleware([isLogin::class, UserAkses::class.':user']);
+Route::get('/get-schedules/{dokter}', [UserSessionControl::class, 'getSchedulesByDoctor'])->middleware([isLogin::class, UserAkses::class.':user']);
+Route::post('/createReservasi', [UserSessionControl::class, 'storeReservasi'])->name('reservasi.store')->middleware([isLogin::class, UserAkses::class.':user']);
+Route::get('/myAccount', [UserSessionControl::class, 'myAccount'])->name('myAccount')->middleware([isLogin::class, UserAkses::class.':user']);
+Route::post('/editAccount', [UserSessionControl::class, 'updateAccount'])->name('account.update')->middleware([isLogin::class, UserAkses::class.':user']);
+Route::get('/detilMedis', [UserSessionControl::class, 'getDetilMedis'])->middleware([isLogin::class, UserAkses::class.':user']);
+Route::get('/detilReservasiMedis', [UserSessionControl::class, 'getDetilReservasiMedis'])->middleware([isLogin::class, UserAkses::class.':user']);
 Route::get('/reservasi', [UserSessionControl::class, 'reservasi'])->middleware([isLogin::class, UserAkses::class.':user']);
 Route::get('/obatUser', [UserSessionControl::class, 'obatUser'])->middleware([isLogin::class, UserAkses::class.':user']);
 
 //dokter
 Route::get('/dokter', [UserSessionControl::class, 'dokter'])->middleware([isLogin::class, UserAkses::class.':dokter']);
+Route::put('/dokterPasien/update', [UserSessionControl::class, 'updatePasien'])->name('dokterPasien.update')->middleware([isLogin::class, UserAkses::class.':dokter']);
+Route::get('/dokterPasien/{id}', [UserSessionControl::class, 'getPasien'])->middleware([isLogin::class, UserAkses::class.':dokter']);
 
 
 
@@ -37,6 +46,7 @@ Route::post('/obat', [halamanAdminController::class, 'storeObat'])->name('obat.s
 Route::post('/createDokter', [halamanAdminController::class, 'storeDokter'])->name('dokter.store')->middleware([isLogin::class, UserAkses::class.':admin']);
 Route::get('/adminDokter/{id}', [halamanAdminController::class, 'getDokter'])->middleware([isLogin::class, UserAkses::class.':admin']);
 Route::put('/adminDokter/update', [halamanAdminController::class, 'updateDokter'])->name('dokter.update')->middleware([isLogin::class, UserAkses::class.':admin']);
+Route::delete('/adminDokter/{id}', [halamanAdminController::class, 'dokterDestroy'])->name('dokter.destroy')->middleware([isLogin::class, UserAkses::class.':admin']);
 Route::put('/obat/update', [halamanAdminController::class, 'update'])->name('obat.update')->middleware([isLogin::class, UserAkses::class.':admin']);
 Route::get('/obat/{id}', [halamanAdminController::class, 'getObat'])->middleware([isLogin::class, UserAkses::class.':admin']);
-Route::delete('/obat/{id}', [halamanAdminController::class, 'destroy'])->name('obat.destroy')->middleware([isLogin::class, UserAkses::class.':admin']);
+Route::delete('/obat/{id}', [halamanAdminController::class, 'obatDestroy'])->name('obat.destroy')->middleware([isLogin::class, UserAkses::class.':admin']);
