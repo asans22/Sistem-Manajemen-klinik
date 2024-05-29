7b0b4a22f71a3473f72cfe7fc1a5b750de7a432a
agil <mughni@mhs.usk.ac.id>
2024-05-30 03:30:46 +0700

    feat: add image and fetch data from database

--- resources/views/user/Dokter.blade.php
+++ resources/views/user/Dokter.blade.php
@@ -4,101 +4,34 @@
 @include ('layouts.component.header', ['pageTitle' => 'Daftar Dokter'], ['description' => 'Informasi daftar dokter'])
 
 <ul class="flex flex-col gap-7 my-16 mx-48">
+    @foreach($doctors as $doctors)
     <li class="flex justify-between items-center py-5 font-poppins">
         <div class="flex items-center gap-5">
-            <img class="rounded-full bg-linen h-24 w-24 flex-shrink-0 mr-10" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+            <!-- <img class="rounded-full bg-linen h-24 w-24 flex-shrink-0 mr-10" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""> -->
+            @if ($doctors->image)
+            <img src="{{ asset('images/' . $doctors->image) }}" alt="Foto Dokter" class="rounded-full bg-linen h-24 w-24 flex-shrink-0 mr-10">
+            @else
+            <!-- Gambar default jika tidak ada gambar -->
+            <img src="{{ url('assets/img/logo.png') }}" alt="Foto Dokter" class="w-20 h-20 object-cover">
+            @endif
             <div class="text-navy">
-                <h2 class="font-bold text-xl">Dr. Emily Johnson</h2>
-                <p class="mt-1 font-light">Cardiologist</p>
-                <p class="mt-2 truncate text-sm font-normal">Specializing in heart health</p>
-                <p class="mt-1 truncate text-sm font-normal">Senin, 08.00 - 12.00 WIB</p>
+                <h2 class="font-bold text-xl">{{ $doctors->name }}</h2>
+                <p class="mt-1 font-light">{{ $doctors->spesialis }}</p>
+                <p class="mt-2 truncate text-sm font-normal">{{ $doctors->deskripsi }}</p>
+                <p class="mt-1 truncate text-sm font-normal">{{ $doctors->jadwal }}</p>
             </div>
         </div>
-        <div class="flex flex-col items-end">
-            <p class="mt-1 text-xs leading-5 text-gray-500">Terakhir dilihat <time datetime="2024-01-23T13:23Z">3j lalu</time></p>
+        
+        <div class="flex flex-col items-end bg-navy rounded-lg hover:bg-blue-sky hover:rounded-lg active:bg-black mt-2 justify-end">
             <div style="margin-left: auto;">
-                <button class="bg-navy rounded-lg hover:bg-blue-sky hover:rounded-lg active:bg-black mt-2 justify-end">
+                <a href="{{ route('reservasiDokter', ['id' => $doctors->id]) }}" class="bg-navy rounded-lg hover:bg-blue-sky hover:rounded-lg active:bg-black mt-2 justify-end">
                     <p class="p-3 text-light">Reservasi</p>
-                </button>
+                </a>
             </div>
         </div>
+
     </li>
-    <li class="flex justify-between items-center py-5 font-poppins">
-        <div class="flex items-center gap-5">
-            <img class="rounded-full bg-linen h-24 w-24 flex-shrink-0 mr-10" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
-            <div class="text-navy">
-                <h2 class="font-bold text-xl">Dr. Emily Johnson</h2>
-                <p class="mt-1 font-light">Cardiologist</p>
-                <p class="mt-2 truncate text-sm font-normal">Specializing in heart health</p>
-                <p class="mt-1 truncate text-sm font-normal">Senin, 08.00 - 12.00 WIB</p>
-            </div>
-        </div>
-        <div class="flex flex-col items-end">
-            <p class="mt-1 text-xs leading-5 text-gray-500">Terakhir dilihat <time datetime="2024-01-23T13:23Z">3j lalu</time></p>
-            <div style="margin-left: auto;">
-                <button class="bg-navy rounded-lg hover:bg-blue-sky hover:rounded-lg active:bg-black mt-2 justify-end">
-                    <p class="p-3 text-light">Reservasi</p>
-                </button>
-            </div>
-        </div>
-    </li>
-    <li class="flex justify-between items-center py-5 font-poppins">
-        <div class="flex items-center gap-5">
-            <img class="rounded-full bg-linen h-24 w-24 flex-shrink-0 mr-10" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
-            <div class="text-navy">
-                <h2 class="font-bold text-xl">Dr. Emily Johnson</h2>
-                <p class="mt-1 font-light">Cardiologist</p>
-                <p class="mt-2 truncate text-sm font-normal">Specializing in heart health</p>
-                <p class="mt-1 truncate text-sm font-normal">Senin, 08.00 - 12.00 WIB</p>
-            </div>
-        </div>
-        <div class="flex flex-col items-end">
-            <p class="mt-1 text-xs leading-5 text-gray-500">Terakhir dilihat <time datetime="2024-01-23T13:23Z">3j lalu</time></p>
-            <div style="margin-left: auto;">
-                <button class="bg-navy rounded-lg hover:bg-blue-sky hover:rounded-lg active:bg-black mt-2 justify-end">
-                    <p class="p-3 text-light">Reservasi</p>
-                </button>
-            </div>
-        </div>
-    </li>
-    <li class="flex justify-between items-center py-5 font-poppins">
-        <div class="flex items-center gap-5">
-            <img class="rounded-full bg-linen h-24 w-24 flex-shrink-0 mr-10" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
-            <div class="text-navy">
-                <h2 class="font-bold text-xl">Dr. Emily Johnson</h2>
-                <p class="mt-1 font-light">Cardiologist</p>
-                <p class="mt-2 truncate text-sm font-normal">Specializing in heart health</p>
-                <p class="mt-1 truncate text-sm font-normal">Senin, 08.00 - 12.00 WIB</p>
-            </div>
-        </div>
-        <div class="flex flex-col items-end">
-            <p class="mt-1 text-xs leading-5 text-gray-500">Terakhir dilihat <time datetime="2024-01-23T13:23Z">3j lalu</time></p>
-            <div style="margin-left: auto;">
-                <button class="bg-navy rounded-lg hover:bg-blue-sky hover:rounded-lg active:bg-black mt-2 justify-end">
-                    <p class="p-3 text-light">Reservasi</p>
-                </button>
-            </div>
-        </div>
-    </li>
-    <li class="flex justify-between items-center py-5 font-poppins">
-        <div class="flex items-center gap-5">
-            <img class="rounded-full bg-linen h-24 w-24 flex-shrink-0 mr-10" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
-            <div class="text-navy">
-                <h2 class="font-bold text-xl">Dr. Emily Johnson</h2>
-                <p class="mt-1 font-light">Cardiologist</p>
-                <p class="mt-2 truncate text-sm font-normal">Specializing in heart health</p>
-                <p class="mt-1 truncate text-sm font-normal">Senin, 08.00 - 12.00 WIB</p>
-            </div>
-        </div>
-        <div class="flex flex-col items-end">
-            <p class="mt-1 text-xs leading-5 text-gray-500">Terakhir dilihat <time datetime="2024-01-23T13:23Z">3j lalu</time></p>
-            <div style="margin-left: auto;">
-                <button class="bg-navy rounded-lg hover:bg-blue-sky hover:rounded-lg active:bg-black mt-2 justify-end">
-                    <p class="p-3 text-light">Reservasi</p>
-                </button>
-            </div>
-        </div>
-    </li>
+    @endforeach
 </ul>
 
 @endsection
