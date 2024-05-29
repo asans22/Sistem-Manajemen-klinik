65c3b82c0f13053b0b9cddc82cabd6505bec023a
agil <mughni@mhs.usk.ac.id>
2024-05-30 03:26:27 +0700

    add styling

--- resources/views/modal/createObat.blade.php
+++ resources/views/modal/createObat.blade.php
@@ -8,7 +8,7 @@
         <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
             <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                 <div class="sm:flex sm:items-start">
-                    <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                    <div class="mt-3 text-center w-full sm:mt-0 sm:text-left">
                         <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-title">
                             Tambah Data Obat
                         </h3>
