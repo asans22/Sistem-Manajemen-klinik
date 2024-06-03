e70263a468c3080f0c52460f22d1d434b1459013
ahmad syah ramadhan <syah22@mhs.usk.ac.id>
2024-06-03 11:49:50 +0700

    filter akun user

--- resources/views/admin/akun.blade.php
+++ resources/views/admin/akun.blade.php
@@ -6,27 +6,29 @@
         <h1 class="font-bold text-3xl">Data Akun</h1>
     </div>
 
-    <div class="pl-10 pt-10 w-[80%] m-auto flex justify-center">
-        <table class="border-collapse border w-full text-center border-black">
+    <div class="pl-10">
+        <table class="box-border bottom-1 text-center">
             <thead class="font-semibold">
                 <tr class="h-10">
-                    <td class="border-2 border-black px-4 py-2">No</td>
-                    <td class="border-2 border-black px-4 py-2">Email</td>
-                    <td class="border-2 border-black px-4 py-2">Nama</td>
-                    <td class="border-2 border-black px-4 py-2">No HP</td>
-                    <td class="border-2 border-black px-4 py-2">Alamat</td>
+                    <td class="border-2 border-black w-16">No</td>
+                    <td class="border-2 border-black w-52">Email</td>
+                    <td class="border-2 border-black w-52">Nama</td>
+                    <td class="border-2 border-black w-52">No HP</td>
+                    <td class="border-2 border-black w-52">Alamat</td>
                 </tr>
             </thead>
             <tbody>
+                @php $angka = 1; @endphp
                 @foreach ($users as $key => $user)
                 @if ($user->role == 'user')
                 <tr class="h-8">
-                    <td class="border-2 border-black">{{ $key + 1 }}</td>
+                    <td class="border-2 border-black">{{ $angka }}</td>
                     <td class="border-2 border-black">{{ $user->email }}</td>
                     <td class="border-2 border-black">{{ $user->name }}</td>
                     <td class="border-2 border-black">{{ $user->no_hp }}</td>
                     <td class="border-2 border-black">{{ $user->alamat   }}</td>
                 </tr>
+                @php $angka++; @endphp
                 @endif
                 @endforeach
             </tbody>
